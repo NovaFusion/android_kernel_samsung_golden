682a56d3f6a8790af01a21ab09e54a96c1371ecc
Marcin Chojnacki <marcinch7@gmail.com>
2014-04-18 19:49:15 +0200

    Revert "fix all section mismatches"
    This reverts commit 10f46d99bd714f596a02e2b752c4e8a7240dfafc.

--- drivers/mfd/dbx500-prcmu.c
+++ drivers/mfd/dbx500-prcmu.c
@@ -753,11 +753,12 @@ static struct platform_driver dbx500_prcmu_driver = {
 		.owner = THIS_MODULE,
 		.name = "dbx500-prcmu",
 	},
+	.probe = dbx500_prcmu_probe,
 };
 
 static int __init dbx500_prcmu_init(void)
 {
-	return platform_driver_probe(&dbx500_prcmu_driver, dbx500_prcmu_probe);
+	return platform_driver_register(&dbx500_prcmu_driver);
 }
 
 MODULE_AUTHOR("Michel JAOUEN <michel.jaouen@stericsson.com>");
